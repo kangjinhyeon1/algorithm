b7ba7c4903dcdcf74b3b7ee529b50db990708d06
Jin hyeon <kangjin627@gmail.com>
2024-03-11 09:15:48 +0900

    3.11

--- 1764.py
+++ 1764.py
@@ -1,23 +1,18 @@
-n, m = map(int, input().split())
+import sys
 
-dic2 = {}
-li = []
-for _ in range(n + m):
-    li.append(str(input()))
+n, m = map(int, sys.stdin.readline().split())
+a = []
+b = []
+for _ in range(n):
+    a.append(str(sys.stdin.readline().rstrip()))
 
-li2 = li[n+2:]
-for key in li2:
-    dic2[key] = dic2.get(key, 0) + 1
+for _ in range(m):
+    b.append(str(sys.stdin.readline().rstrip()))
 
-result = []
-print(dic2)
-# for i in li:
-#     if dic2[i] == None:
-#         print(dic2[i])
-# if dic2[i] != None:
-#     result.append(i[0])
-# else:
-#     a = 0
-# print(len(result))
-# for i in result:
-#     print(i)
+result = list(set(a) & set(b))
+result.sort()
+
+print(len(result))
+
+for i in result:
+    print(i)
